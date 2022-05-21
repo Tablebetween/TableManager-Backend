aaa7fef47859f4a380508da66044505a722f27ea
parkjunsun <qkrwnstns52@naver.com>
2022-05-21 22:29:00 +0900

    2022.05.21 TableManager 프로젝트 생성

---
+++ src/test/java/heesoon/tableManager/TableManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package heesoon.tableManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
