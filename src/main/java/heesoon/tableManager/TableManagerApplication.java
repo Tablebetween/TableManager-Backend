6d1bab54df6e30d65a0a43dc41d29745ec8bee4e
wonhecho <chowond02@naver.com>
2022-06-21 03:01:07 +0900

    21-06-21 S3 update

--- src/main/java/heesoon/tableManager/TableManagerApplication.java
+++ src/main/java/heesoon/tableManager/TableManagerApplication.java
@@ -2,12 +2,31 @@ package heesoon.tableManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+// 테스트용 RestController
+@RestController
 @SpringBootApplication
 public class TableManagerApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(TableManagerApplication.class, args);
-	}
+		new SpringApplicationBuilder(TableManagerApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 
+	}
+	// 테스트용 RestController
+	@GetMapping
+	public String home()
+	{
+		return new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+	}
 }
